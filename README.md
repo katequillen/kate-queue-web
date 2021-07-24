6552927751d51b16c5bdefcbc2d6927f41090027
katequillen <kate.quillen@gmail.com>
2021-07-24 16:36:02 +0200

    added popups file

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
-<!--
-*** Thanks for checking out the Best-README-Template. If you have a suggestion
-*** that would make this better, please fork the repo and create a pull request
-*** or simply open an issue with the tag "enhancement".
-*** Thanks again! Now go create something AMAZING! :D
--->
-
 <!-- PROJECT SHIELDS -->
 <!--
 *** I'm using markdown "reference style" links for readability.
@@ -13,24 +6,10 @@
 *** for contributors-url, forks-url, etc. This is an optional, concise syntax you may use.
 *** https://www.markdownguide.org/basic-syntax/#reference-style-links
 -->
-<!--[![Contributors][contributors-shield]][contributors-url]
-[![Forks][forks-shield]][forks-url]
-[![Stargazers][stars-shield]][stars-url]
-[![Issues][issues-shield]][issues-url]
-[![MIT License][license-shield]][license-url]
-[![LinkedIn][linkedin-shield]][linkedin-url]
-
-
-
 <!-- PROJECT LOGO -->
 <!--
 <br />
-<p align="center">
-  <a href="https://github.com/othneildrew/Best-README-Template">
-    <img src="images/logo.png" alt="Logo" width="80" height="80">
-  </a>
-
-  <h3 align="center">Best-README-Template</h3>
+  <h3 align="center">Welcome to my personal website README!</h3>
 
   <p align="center">
     An awesome README template to jumpstart your projects!
@@ -78,6 +57,7 @@
 ## About The Project
 
 [![Product Name Screen Shot][product-screenshot]](https://example.com)
+
 
 There are many great README templates available on GitHub, however, I didn't find one that really suit my needs so I created this enhanced one. I want to create a README template so amazing that it'll be the last one you ever need -- I think this is it.
 
---
+++ src/js/popups.js
@@ -0,0 +1,57 @@
+"use strict";
+
+//collapsible
+const cdesign = document.querySelector(".js-cdesign");
+const cfill = document.querySelector(".js-cfill");
+const cshare = document.querySelector(".js-cshare");
+
+const contentDesign = document.querySelector(".js-design");
+const contentFill = document.querySelector(".js-fill");
+const contentShare = document.querySelector(".js-share");
+const arrowShare = document.querySelector(".js-arrowS");
+
+contentFill.classList.add("hidden");
+contentShare.classList.add("hidden");
+arrowShare.classList.remove("collapsible__icon2");
+arrowShare.classList.add("arrowhidden");
+console.log(arrowShare);
+
+function hiddenDesign() {
+  contentDesign.classList.add("hidden");
+  //arrowDesign.classList.add('arrowhidden');
+}
+function hiddenFill() {
+  contentFill.classList.remove("hidden");
+}
+function hiddenShare() {
+  contentShare.classList.remove("hidden");
+}
+
+function showDesign() {
+  hiddenDesign();
+  if (contentDesign.classList.contains("js-design")) {
+    contentFill.classList.add("hidden");
+    contentShare.classList.add("hidden");
+    contentDesign.classList.remove("hidden");
+  }
+}
+
+function showFill() {
+  hiddenFill();
+  if (contentFill.classList.contains("js-fill")) {
+    contentDesign.classList.add("hidden");
+    contentShare.classList.add("hidden");
+  }
+}
+
+function showShare() {
+  hiddenShare();
+  if (contentShare.classList.contains("js-share")) {
+    contentDesign.classList.add("hidden");
+    contentFill.classList.add("hidden");
+  }
+}
+
+cdesign.addEventListener("click", showDesign);
+cfill.addEventListener("click", showFill);
+cshare.addEventListener("click", showShare);
